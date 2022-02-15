78d31796e50e84f188f6429b2780a3140d40db14
abraham <apolichenco@gmail.com>
2022-02-14 22:44:15 -0500

    commit tree 3af234670e6961a0e32becd146ab8f7b26574cb2

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(x, y) {
+return x + y;
+}
+function subtract(x, y) {
+return x - y;
+}
+function multiply(x, y) {
+return x * y;
+}
+function divide(x, y) {
+return x / y;
+}
+function increment(number) {
+    return (number += 1);
+}
+function decrement(number) {
+    return (number -= 1);
+}
+function makeInt(number){
+  return  parseInt(number, 10);
+}
+function preserveDecimal(number) {
+    return parseFloat(number);
+}
